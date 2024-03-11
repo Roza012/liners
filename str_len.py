a66db849b9bdbf27104a121b8d3803307b9794c0
Roza <hadidroza@gmail.com>
2024-03-11 11:37:16 +0200

    change print to return

--- str_len.py
+++ str_len.py
@@ -7,8 +7,8 @@ Date: 11.3.2024
 
 def str_len(lst: list):
     """
-    Gets a variable of list type and print the length of all the words in it.
+    Gets a variable of list type and return the length of all the words in it.
     :param lst: List that the function receive.
     """
     new_list = [len(member) for member in lst]
-    print(new_list)
+    return new_list
